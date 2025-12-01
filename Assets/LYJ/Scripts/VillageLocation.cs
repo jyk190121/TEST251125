25fbf5af6f5d246335b436ddb03895de10491534
galeum369-tech <galeum369@gmail.com>
2025-12-01 14:57:51 +0900

    1

--- Assets/LYJ/Scripts/VillageLocation.cs
+++ Assets/LYJ/Scripts/VillageLocation.cs
@@ -117,12 +117,12 @@ public class VillageLocation : MonoBehaviour
     {
         Debug.Log($"{locationName} 상호작용");
 
-       /*  UIManager를 통해 패널 열기
+         UIManager를 통해 패널 열기
         if (MasterManager.Instance != null)
         {
             MasterManager.Instance.UIManager.OpenPanel(panelName);
             ShowInteractionPrompt(false);  // 프롬프트 숨기기
-        }*/
+        }
     }
 
     public LocationType GetLocationType() => locationType;
