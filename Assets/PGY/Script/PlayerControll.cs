e3f078c541b22de63bd42e4a06be0f073475c11c
GoldPan-ChoC <pky202@gmail.com>
2025-12-01 14:28:37 +0900

    -스크립트추가
    -DataManager
    -KeyManager -> 단축키 설정
    -PlayerControll -> 플레이어 행동 스크립트
    -PlayerModel
    -PlayerMove

---
+++ Assets/PGY/Script/PlayerControll.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+//PlayerMove에서 입력받은 값에 따라 실행되는 함수를 정리한 스크립트
+public class PlayerControll : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(Vector3 dir)
+    {
+
+    }
+
+    public void Attack()
+    {
+
+    }
+
+    public void SpecialAttack()
+    {
+
+    }
+
+    public void Roll()
+    {
+
+    }
+
+    public void Die()
+    {
+
+    }
+}
