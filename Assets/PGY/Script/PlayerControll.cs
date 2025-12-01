30a7d878197ed40ced233da6ac695fece50aa0ca
GoldPan-ChoC <pky202@gmail.com>
2025-12-01 14:49:28 +0900

    스크립트 수정
    DataManger, PlayerControll, PlayerModel

--- Assets/PGY/Script/PlayerControll.cs
+++ Assets/PGY/Script/PlayerControll.cs
@@ -3,21 +3,24 @@ using UnityEngine;
 //PlayerMove에서 입력받은 값에 따라 실행되는 함수를 정리한 스크립트
 public class PlayerControll : MonoBehaviour
 {
+    Rigidbody rb;
+    PlayerModel model;
 
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        model = DataManager.Instance.GetPlayer();
     }
 
     public void Move(Vector3 dir)
     {
-
+        rb.linearVelocity = dir * model.moveSpeed;
     }
 
     public void Attack()
