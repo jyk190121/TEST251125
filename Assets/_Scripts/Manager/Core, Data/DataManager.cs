023b6d83ceed2bf5e2426c0949f52a36ff0fae83
jyk190121 <46877384+jyk190121@users.noreply.github.com>
2025-12-06 05:22:28 +0900

    미사용변수제거
    1. 데이터매니저 미사용변수로 에러발생
     - int aspd 제거

--- Assets/_Scripts/Manager/Core, Data/DataManager.cs
+++ Assets/_Scripts/Manager/Core, Data/DataManager.cs
@@ -19,7 +19,7 @@ public class DataManager : MonoBehaviour
     }
 
     //플레이어 아이템 장착시 스탯 변경
-    public void playerStatChanged(int att, int def, int hp, int mspd = 0, int aspd)
+    public void playerStatChanged(int att, int def, int hp, int mspd = 0)
     {
         //이후 장비 관련 변수 추가시 수정 필요
         player.HP = modelstat.HP + hp;
