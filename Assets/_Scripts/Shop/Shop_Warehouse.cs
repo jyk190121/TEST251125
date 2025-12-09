88e896af41bd7b03c76afa76db022b972a4d017d
jyk190121 <46877384+jyk190121@users.noreply.github.com>
2025-12-10 07:05:14 +0900

    1차커밋
    251210
    어제 작업한거 날아갔네~~ㅇ.ㅇ

---
+++ Assets/_Scripts/Shop/Shop_Warehouse.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Warehouse : MonoBehaviour
+{
+
+    float keyDownTime;           // 상호작용을 위한 시간
+
+    bool playerIn;             // 플레이어가 창고에 닿았는지 여부
+    float keyTimer;             // 키 입력 시간
+    public Image image;         // 키입력하는 동안 띄울 이미지
+
+    PlayerController player;    // 플레이어 스크립트( 임시 )
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        keyDownTime = 1.5f;
+        keyTimer = 0f;
+        image.gameObject.SetActive(false);
+        image.fillAmount = 0f;
+        image.color = new Color(0, 0, 150f, 50f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 플레이어가 창고 근처에 있으면 상호작용 키 누름 시간 체크
+        if (playerIn)
+        {
+            if (Input.GetKey(KeySetting.keys[KeyInput.INTERACTIVE]))
+            {
+                keyTimer += Time.deltaTime;
+                image.gameObject.SetActive(true);
+                image.fillAmount = keyTimer;
+
+                if (keyTimer >= keyDownTime)
+                {
+                    image.gameObject.SetActive(false);
+                    //창고개방
+                    print("창고개방");
+                }
+            }
+            else
+            {
+                keyTimer = 0f;
+                //image.fillAmount = 0f;
+                image.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            playerIn = true;
+            player = collision.collider.GetComponent<PlayerController>();
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            playerIn = false;
+            keyTimer = 0f;
+            player = null;
+            image.gameObject.SetActive(false);
+        }
+    }
+}
