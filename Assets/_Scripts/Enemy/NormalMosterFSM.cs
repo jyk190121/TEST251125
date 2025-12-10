c4d3e400ab01c08315a753fec5db9e7daf895466
galeum369-tech <galeum369@gmail.com>
2025-12-10 15:48:55 +0900

    일단 코드

--- Assets/_Scripts/Enemy/NormalMosterFSM.cs
+++ Assets/_Scripts/Enemy/NormalMosterFSM.cs
@@ -9,7 +9,7 @@ public class NormalMosterFSM : MonoBehaviour
         Idle,
         Move,
         Attack,
-        GetHit,  //쓸지는 모르겠다만 애니메이션은 있기에
+        GetHit,  //내부에서 데미지 처리하면 될듯
         Die
     }
 
@@ -26,10 +26,36 @@ public class NormalMosterFSM : MonoBehaviour
     float def;
     //몬스터 공격 딜레이
     float coolTime;
-    float timer = 0;
+    float timer = 0f;
     //몬스터 공격/인식 범위
     float attRange;
     float detRange;
+
+    Type monsterType;   //몬스터 Melee/Range
+    Race monsterRace;   //몬스터 종족
+
+    MonsterPattern[] patterns;  //패턴 목록
+
+    //몬스터 공격 세부
+    float windupTime;  //공격준비(바람잡기)
+    float recoveryTime; //공격 후딜
+
+    //세부 - 근접
+    float meleeRadius;  //데이터에서 attackRadius(공격 거리
+    float meleeAngle;   //데이터에서 attackAngle(공격 각도(?))
+    float aoeRange;     //데이터에서 aoeRange(범위)
+    float aoeDamageMuliplier;
+
+    //세부 - 원거리
+    float projectileSpeed;
+    int projectilCount;
+    float shotInterval;
+    int burstCount;
+
+    //FX관련
+    GameObject attackFX;
+    GameObject hitFX;
+    GameObject deathFX;
 
     NavMeshAgent agent;
 
@@ -40,19 +66,47 @@ public class NormalMosterFSM : MonoBehaviour
         state = MonsterState.Idle;
 
         target = GameObject.FindWithTag("Player").transform;
-
         anim = GetComponentInChildren<Animator>();
-
         agent = GetComponent<NavMeshAgent>();
         agent.enabled = false;
 
+        //기본스탯
         currentHP = monsterData.HP;
         att = monsterData.Attack;
         speed = monsterData.Speed;
         def = monsterData.Defense;
+        //쿨
         coolTime = monsterData.CoolTime;
+        //범위
         attRange = monsterData.attackRange;
         detRange = monsterData.detectionRange;
+
+        //몬스터 정보
+        monsterType = monsterData.Type;
+        monsterRace = monsterData.Race;
+        patterns = monsterData.Pattern;
+        //패턴 파라미터
+        windupTime = monsterData.windupTime;
+        recoveryTime = monsterData.recoveryTime;
+
+        meleeRadius = monsterData.attackRadius;
+        meleeAngle = monsterData.attackAngle;
+        aoeRange = monsterData.aoeRange;
+        aoeDamageMultiplier = monsterData.aoeDamageMultiplier;
+
+        projectileSpeed = monsterData.projectileSpeed;
+        projectileCount = monsterData.projectileCount;
+        shotInterval = monsterData.shotInterval;
+        burstCount = monsterData.burstCount;
+
+        // FX
+        attackFX = monsterData.attackFX;
+        hitFX = monsterData.hitFX;
+        deathFX = monsterData.deathFX;
+
+        // Agent Speed
+        agent.speed = speed;
+
     }
 
     // Update is called once per frame
