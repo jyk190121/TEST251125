afd831012594617523f144da7b4fa20a44bcc1c2
rladudcks78 <rladudcks623@gmail.com>
2025-12-15 17:49:14 +0900

    251215
    17:49

--- Assets/_Scripts/Damage/TestMonster.cs
+++ Assets/_Scripts/Damage/TestMonster.cs
@@ -10,13 +10,18 @@ public class TestMonster : MonoBehaviour, IHitResponder
     // 몬스터의 레이어 설정 (Unity Inspector에서 9번 레이어로 설정해야 합니다.)
     private const int MonsterLayer = 11;
 
+    private RoomController room;
+
     private void Awake()
     {
         // 몬스터 오브젝트의 레이어를 설정해주는 것이 좋습니다.
         // Hierarchy에서 이 오브젝트를 선택하고 Inspector에서 Layer를 9번(Monster)으로 설정해야 합니다.
         gameObject.layer = MonsterLayer;
         Debug.Log("몬스터 레이어 지정");
+
+        room = GetComponentInParent<RoomController>();
     }
+
 
     // IHitResponder 인터페이스 구현
     public void TakeDamage(DamageData data)
@@ -40,6 +45,12 @@ public class TestMonster : MonoBehaviour, IHitResponder
     public void Die()
     {
         Debug.Log("몬스터 사망!");
+
+        if (room != null)
+        {
+            room.ClearDungeon(gameObject);
+        }
+
         // 테스트를 위해 오브젝트를 비활성화합니다.
         gameObject.SetActive(false);
         // 실제 게임에서는 파티클, 애니메이션, 드롭 아이템 등의 처리가 필요합니다.
