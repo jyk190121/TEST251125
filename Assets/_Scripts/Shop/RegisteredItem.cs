4575ba4ad0cd04b9e2762674283cb260d442931d
jyk190121 <46877384+jyk190121@users.noreply.github.com>
2025-12-16 15:11:44 +0900

    commit
    1. 디벨롭 가져오기 전 커밋 1차

--- Assets/_Scripts/Shop/RegisteredItem.cs
+++ Assets/_Scripts/Shop/RegisteredItem.cs
@@ -1,6 +1,28 @@
 using UnityEngine;
-
+/// <summary>
+/// 인벤토리에 있는 아이템 팔기
+///  - 인벤토리 아이템 리스트
+///  - 등록 시 아이템 갯수 제거 <-> 등록된 아이템 추가
+///  
+/// </summary>
 public class RegisteredItem : MonoBehaviour
 {
+    //등록된 아이템 리스트
     public Item[] itemList;
+    
+    //인벤토리 아이템 리스트
+    InventoryManager inventory;
+
+    private void Start()
+    {
+        inventory = _MasterManager.Instance.InventoryManager;
+    }
+    
+    public void InventoryList()
+    {
+        inventory.
+    }
+
+    
+
 }
