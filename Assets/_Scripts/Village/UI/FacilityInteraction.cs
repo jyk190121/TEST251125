b042495c27a52aa4098383104b4981403905ba35
reasonjeong614-dotcom <reasonjeong614@gmail.com>
2025-12-16 16:22:57 +0900

    세미 완성

--- Assets/_Scripts/Village/UI/FacilityInteraction.cs
+++ Assets/_Scripts/Village/UI/FacilityInteraction.cs
@@ -17,7 +17,6 @@ public class FacilityInteraction : MonoBehaviour
     [SerializeField] private TextMeshProUGUI keyText;
     [SerializeField] private TextMeshProUGUI promptUIText;
     [SerializeField] private GameObject promptPanel;
-    [SerializeField] GameObject openPleasePanel;
 
     private Transform playerTransform;
     private bool isPlayerInRange = false;
