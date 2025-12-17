d66ee517a5bd9b3cca1cc1eee104e141fa46310b
GoldPan-ChoC <pky202@gmail.com>
2025-12-17 14:46:12 +0900

    BattleRecord 수정

--- Assets/_Scripts/Dungeon/BattleRecord.cs
+++ Assets/_Scripts/Dungeon/BattleRecord.cs
@@ -23,16 +23,12 @@ public class BattleRecord: MonoBehaviour
     TextMeshProUGUI itemCount;
     TextMeshProUGUI MonsterCount;
 
-    
+    Transform ItemList;
+    Transform MonsterList;
 
     void Start()
     {
         _MasterManager.Instance.DataManager.RegisterBattleRecord(this);
-    }
-
-    void Update()
-    {
-        
     }
 
     // 리스트에 몬스터 추가
@@ -48,6 +44,9 @@ public class BattleRecord: MonoBehaviour
 
     public void OpenResultPanel(bool Clear, bool Pendent)
     {
+        //결과창 케이스 별 분리
+        resultPanel.SetActive(true);            // 평소엔 꺼놨다가 키기
+        retry.gameObject.SetActive(false);      // retry는 사망 시에만
         if (Pendent)
         {
             deadReason.text = "펜던트로 탈출함";
@@ -58,10 +57,29 @@ public class BattleRecord: MonoBehaviour
         }
         else
         {
+            retry.gameObject.SetActive(true);
             deadReason.text = "사고로 사망";
+            retry.text = $"{KeySetting.keys[KeyInput.INTERACTIVE]}   다시 플레이";
         }
 
         goToVillage.text = $"{KeySetting.keys[KeyInput.CANCLE]}   마을로 가기";
-        retry.text = $"";
+        
+        itemCount.text = items.Count.ToString();
+        MonsterCount.text = KilledMonster.Count.ToString();
+
+        // 획득한 아이템 표시
+        foreach(var item in items)
+        {
+            GameObject obj = item.itemPrefab;
+            Instantiate(obj, ItemList);
+        }
+
+        // 잡은 몬스터 표시
+        foreach(var monster in KilledMonster)
+        {
+            GameObject obj = monster.MobPrefab;
+            Instantiate(obj, MonsterList);
+        }
+
     }
 }
