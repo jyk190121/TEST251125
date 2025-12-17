a0cbcf7e27be8b6b74b1e9577f2cb1eb2f2a1e1c
rladudcks78 <rladudcks623@gmail.com>
2025-12-17 16:48:32 +0900

    251217
    16:48

--- Assets/_Scripts/Dungeon/BossPortal.cs
+++ Assets/_Scripts/Dungeon/BossPortal.cs
@@ -61,14 +61,13 @@ public class BossPortal : MonoBehaviour
 
     private void ReturnToTown()
     {
-        SceneManager.LoadScene(SceneName);
         // 데이터 저장 및 상태 변경
         DataManager dataManager = _MasterManager.Instance.DataManager;
         if (dataManager != null)
         {
             dataManager.ChangeReturn(true);
         }
-
+        SceneManager.LoadScene(SceneName);
         Debug.Log("3초 유지 완료! 마을로 이동합니다.");
     }
 
@@ -77,7 +76,7 @@ public class BossPortal : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             isPlayerIn = true;
-            Debug.Log("포탈 진입: T 키를 3초간 누르세요.");
+            Debug.Log("포탈 진입: G 키를 3초간 누르세요.");
             key.gameObject.SetActive(true);
         }
     }
