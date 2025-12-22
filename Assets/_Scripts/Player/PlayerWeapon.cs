9bd9b102991c3c72fc2aa932fc69e11d98c3e0aa
GoldPan-ChoC <pky202@gmail.com>
2025-12-22 15:58:06 +0900

    무기 장착 해제 완전본
    일걸요

--- Assets/_Scripts/Player/PlayerWeapon.cs
+++ Assets/_Scripts/Player/PlayerWeapon.cs
@@ -18,16 +18,21 @@ public class PlayerWeapon : MonoBehaviour
         DataManager.OnEquipmentChanged -= WeaponPrefab;
     }
 
+    private void Start()
+    {
+        WeaponPrefab();
+    }
+
     void WeaponPrefab()
     {
         weapon = _MasterManager.Instance.DataManager.GetWeapon();
-        if (weapon == null) return;
         foreach (var weaponoff in weaponlist)
         {
             weaponoff.SetActive(false);
         }
+        if (weapon == null) return;
         //검일때 검/방패
-        if(weapon.itemID == 2001)
+        if (weapon.itemID == 2001)
         {
             weaponlist[0].SetActive(true);
             weaponlist[4].SetActive(true);
