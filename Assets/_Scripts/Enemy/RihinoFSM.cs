eeda42895271beaa6db0a60b99647e3dff8e83d0
jyk190121 <46877384+jyk190121@users.noreply.github.com>
2025-12-25 15:55:09 +0900

    최종본 (제출)
    커밋

--- Assets/_Scripts/Enemy/RihinoFSM.cs
+++ Assets/_Scripts/Enemy/RihinoFSM.cs
@@ -142,7 +142,7 @@ public class RihinoFSM : MonoBehaviour ,IHitResponder
     void Idle()
     {
         anim.applyRootMotion = false;
-        agent.isStopped = true;
+        //agent.isStopped = true;
         anim.SetBool("isMove", false);
 
         if (target == null)
